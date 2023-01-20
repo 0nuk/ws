27b5a7c2c706b6508160d69d8974adce3569ee4c
Tardes <Tardes@GET-05>
2023-01-20 20:50:43 +0100

    20.01

--- 00_Teoria/src/test/NumerosRomanos.java
+++ 00_Teoria/src/test/NumerosRomanos.java
@@ -2,72 +2,54 @@ package test;
 
 public class NumerosRomanos {
 	public static void main(String[] args) {
-		System.out.println(romano("XIX"));
+		System.out.println(romano("XCIV"));
 	}
+	
 	public static int romano(String cadena) {
 		int numRom = 0;	
-		int numRomAnterior = 0;
+		int numRomSiguiente = 0;
 		int resu = 0;
-		int acumRom = 0;
+		
 		for (int i = 0; i < cadena.length(); i++) {
-			switch (cadena.charAt(i)) {
-				case 'I':
-					numRom = 1;
-					break;
-				case 'V':
-					numRom = 5;
-					break;
-				case 'X':
-					numRom = 10;
-					break;
-				case 'L':
-					numRom = 50;
-					break;
-				case 'C':
-					numRom = 100;
-					break;
-				case 'D':
-					numRom = 500;
-					break;
-				case 'M':
-					numRom = 1000;
-					break;
-			}
-			if(numRom==numRomAnterior) {
-				resu += numRom;
-				numRomAnterior = numRom; 
-			} else {
-				numRomAnterior = numRom;
-				while(numRom == numRomAnterior) {
-					acumRom += numRom;
-					i++;
-					switch (cadena.charAt(i)) {
-					case 'I':
-						numRom = 1;
-						break;
-					case 'V':
-						numRom = 5;
-						break;
-					case 'X':
-						numRom = 10;
-						break;
-					case 'L':
-						numRom = 50;
-						break;
-					case 'C':
-						numRom = 100;
-						break;
-					case 'D':
-						numRom = 500;
-						break;
-					case 'M':
-						numRom = 1000;
-						break;
-					}
+			numRom = conver(cadena.charAt(i));
+			if ((i+1) < cadena.length()) {
+				numRomSiguiente = conver(cadena.charAt(i+1));
 				}
-				resu += (numRom-acumRom);
-			}
-		}			
+				if(numRom>=numRomSiguiente) {
+					resu += numRom;
+				} else {
+					resu += ((numRomSiguiente) - numRom);
+					i ++;
+				}
+		}
 		return resu;
 	}
+	
+	public static int conver(char roma) {
+		int numRom = 0;
+		switch (roma) {
+		case 'I':
+			numRom = 1;
+			break;
+		case 'V':
+			numRom = 5;
+			break;
+		case 'X':
+			numRom = 10;
+			break;
+		case 'L':
+			numRom = 50;
+			break;
+		case 'C':
+			numRom = 100;
+			break;
+		case 'D':
+			numRom = 500;
+			break;
+		case 'M':
+			numRom = 1000;
+			break;	
+		}
+		return numRom;
+	}	
 }

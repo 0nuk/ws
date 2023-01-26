62ffaf8162a9de9a5749ca6433bee33237d41d14
Tardes <Tardes@GET-05>
2023-01-26 17:57:53 +0100

    26.01

--- P02_Ejercicios/src/ejercicio06/Ejercicio03.java
+++ P02_Ejercicios/src/ejercicio06/Ejercicio03.java
@@ -3,9 +3,10 @@
 package ejercicio06;
 public class Ejercicio03 {
 	public static void main(String[] args) {
-		int[] nums = {1, 2, 3, 4, 5};
+		int[] nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 		Ejercicio01.print(nums);
 		Ejercicio01.print(reves(nums));
+	
 	}
 	public static int[] reves(int[] nums) {
 		int[] valores = new int[nums.length];

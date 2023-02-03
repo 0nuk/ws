58f2adfc5e74fd54d9671e00aa996b41c62ef94b
Tardes <Tardes@GET-05>
2023-02-03 20:03:59 +0100

    03.02

--- P03_Figuras_v3/src/model/Cuadrado.java
+++ P03_Figuras_v3/src/model/Cuadrado.java
@@ -7,4 +7,18 @@ public class Cuadrado extends Rectangulo {
 	public String toString() {
 		return "Cuad[" + getX() + ", " + getY() + "; " + getL1() + ", " + "]";
 	}
+	
+	public double getLado() {
+		return getL1();
+	}
+	public void setLado(double lado) {
+		setL1(lado);
+		setL2(lado);
+	}
+	public void setL1(double l1) {
+		setLado(l1);
+	}
+	public void setL2(double l2) {
+		setLado(l2);
+	}
 }

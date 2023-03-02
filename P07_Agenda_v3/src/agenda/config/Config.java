b648a8d54dd7005022b31b20d8754664e300b39d
Tardes <Tardes@GET-05>
2023-03-02 20:57:28 +0100

    02.03

--- P07_Agenda_v3/src/agenda/config/Config.java
+++ P07_Agenda_v3/src/agenda/config/Config.java
@@ -4,7 +4,13 @@ import agenda.persistencia.ContactoDao;
 import agenda.persistencia.ContactoDaoSerial;
 
 public class Config {
+	
+	private static ContactoDao cDao; 
+	
 	public static ContactoDao getContactoDao() {
-		return new ContactoDaoSerial();
+		if (cDao == null) {
+			 cDao = new ContactoDaoSerial();
+		}
+		return cDao;
 	}
 }

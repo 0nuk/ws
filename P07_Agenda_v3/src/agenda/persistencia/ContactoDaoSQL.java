d32e74bf9b5e8e178534280ebc291dff50673e56
Tardes <Tardes@GET-05>
2023-03-10 20:25:44 +0100

    10.03

--- P07_Agenda_v3/src/agenda/persistencia/ContactoDaoSQL.java
+++ P07_Agenda_v3/src/agenda/persistencia/ContactoDaoSQL.java
@@ -74,10 +74,7 @@ public class ContactoDaoSQL implements ContactoDao{
 			} else {
 				con.rollback();
 				throw new PersistenciaException();
-			}
-			
-						
-		
+			}	
 		} catch (SQLException e) {
 			e.printStackTrace();
 			throw new PersistenciaException();
